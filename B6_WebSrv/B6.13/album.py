ace6083f9c8260cba2889b5c643b938b5b1bfee6
stalkspectrum <55536946+stalkspectrum@users.noreply.github.com>
2019-11-25 22:29:58 +0800

    B6.13 fix 1

--- B6_WebSrv/B6.13/album.py
+++ B6_WebSrv/B6.13/album.py
@@ -2,39 +2,46 @@ import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
-DB_PATH = "sqlite:///albums.sqlite3"
+DB_PATH = 'sqlite:///albums.sqlite3'
 Base = declarative_base()
 
-
 class Album(Base):
-    """
-    Описывает структуру таблицы album для хранения записей музыкальной библиотеки
-    """
-
-    __tablename__ = "album"
-
+    __tablename__ = 'album'
     id = sa.Column(sa.INTEGER, primary_key=True)
     year = sa.Column(sa.INTEGER)
     artist = sa.Column(sa.TEXT)
     genre = sa.Column(sa.TEXT)
     album = sa.Column(sa.TEXT)
 
-
 def connect_db():
-    """
-    Устанавливает соединение к базе данных, создает таблицы, если их еще нет и возвращает объект сессии 
-    """
-    engine = sa.create_engine(DB_PATH)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(engine)
-    return session()
+    DB_CONNECTOR = sa.create_engine(DB_PATH)
+    Base.metadata.create_all(DB_CONNECTOR)
+    FACTORY_SESSIONS = sessionmaker(DB_CONNECTOR)
+    return FACTORY_SESSIONS()
 
+def show_all():
+    SESSION_S = connect_db()
+    ALBUMS_ALL = SESSION_S.query(Album).all()
+    return ALBUMS_ALL
 
-def find(artist):
-    """
-    Находит все альбомы в базе данных по заданному артисту
-    """
-    session = connect_db()
-    albums = session.query(Album).filter(Album.artist == artist).all()
+def find_artist(artist):
+    SESSION_F = connect_db()
+    albums = SESSION_F.query(Album).filter(Album.artist == artist).all()
     return albums
+
+''' НЕПРАВИЛЬНО
+def if_already_exist(ARTIST_, ALBUM_):
+    SESSION_I = connect_db()
+    ID_ = select([Album.id]).where(Album.album == ALBUM_)
+    if select([Album.artist]).where(Album.id == ID_) == ARTIST_:
+        return True
+    else:
+        return False
+'''
+
+def add_album(YEAR_F, ARTIST_F, GENRE_F, ALBUM_F):
+    new_album_data = Album(year=YEAR_F, artist=ARTIST_F, genre=GENRE_F, album=ALBUM_F)
+    SESSION_W = connect_db()
+    SESSION_W.add(new_album_data)
+    SESSION_W.commit()
+    return 'NEW Album added to DATABASE!'
--- B6_WebSrv/B6.13/server.py
+++ B6_WebSrv/B6.13/server.py
@@ -4,17 +4,50 @@ from bottle import HTTPError
 from bottle import request
 import album
 
-@route("/albums/<artist>")
-def albums(artist):
-    albums_list = album.find(artist)
-    if not albums_list:
-        message = "Альбомов {} не найдено".format(artist)
-        result = HTTPError(404, message)
-    else:
-        album_names = [album.album for album in albums_list]
-        result = "Список альбомов {}: ".format(artist)
-        result += ", ".join(album_names)
-    return result
+@route('/')
+def server_root():
+    with open('index.html', 'r', encoding='UTF-8') as INDEX_FILE:
+        INDEX_HTML = INDEX_FILE.read()
+    return INDEX_HTML
 
-if __name__ == "__main__":
+@route('/albums', method='POST')
+def add_new_album():
+    YEAR_N = request.forms.get('year')
+    ARTIST_N = request.forms.get('artist')
+    GENRE_N = request.forms.get('genre')
+    ALBUM_N = request.forms.get('album')
+    RES_N = album.add_album(YEAR_N, ARTIST_N, GENRE_N, ALBUM_N)
+    RES_D = '<P><A HREF="/">Вернуться на Home Page</A></P>'
+    RES_D += 'Альбом <B>' + ARTIST_N + ' -- ' + ALBUM_N +'</B> добавлен в базу.'
+    return RES_D
+
+@route('/albums/<artist>')
+def albums_count(artist):
+    if artist == 'all':
+        ALBUM_LIST = album.show_all()
+        ALL_ALBUM_BASE = []
+        for ALBUM_S in ALBUM_LIST:
+            ALBUM_STRING = ALBUM_S.artist + ' -- ' + ALBUM_S.album
+            ALL_ALBUM_BASE.append(ALBUM_STRING)
+        ALBUM_SUM = len(ALL_ALBUM_BASE)
+        RES_A = '<P><A HREF="/">Вернуться на Home Page</A></P>'
+        RES_A += 'Всего в базе найдено <B>' + str(ALBUM_SUM) + '</B> альбомов:<BR><OL><LI>'
+        RES_A += '</LI><LI>'.join(ALL_ALBUM_BASE)
+        RES_A += '</LI>'
+        return RES_A
+    else:
+        ALBUM_LIST = album.find_artist(artist)
+        if not ALBUM_LIST:
+            MSG_B = 'Альбомов {} не найдено'.format(artist)
+            RES_B = HTTPError(404, MSG_B)
+        else:
+            ALBUM_NAMES = [album.album for album in ALBUM_LIST]
+            ALBUM_SUM = len(ALBUM_NAMES)
+            RES_B = '<P><A HREF="/">Вернуться на Home Page</A></P>'
+            RES_B += 'В базе найдено <B>{}</B> альбомов <B>{}</B>:<BR><OL><LI>'.format(ALBUM_SUM, artist)
+            RES_B += '</LI><LI>'.join(ALBUM_NAMES)
+            RES_B += '</LI>'
+        return RES_B
+
+if __name__ == '__main__':
     run(host='localhost', port=8080, debug=True)
