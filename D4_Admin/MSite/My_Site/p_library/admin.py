cb84c32bb441873e02ce5050d46a52e4d83d5a99
stalkspectrum <55536946+stalkspectrum@users.noreply.github.com>
2020-02-06 22:35:05 +0800

    D4.11 fix 10

--- D4_Admin/MSite/My_Site/p_library/admin.py
+++ D4_Admin/MSite/My_Site/p_library/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
 from p_library.models import Author, Book
 
 @admin.register(Author)
@@ -9,4 +7,8 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    @staticmethod
+    def author_full_name(obj):
+        return obj.author.full_name
+    list_display = ('title', 'author_full_name')
+    fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'price')
--- D4_Admin/MSite/My_Site/p_library/views.py
+++ D4_Admin/MSite/My_Site/p_library/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
+from django.shortcuts import redirect
 from p_library.models import Book
 
 def books_list(request):
@@ -19,4 +19,37 @@ def index(request):
         'books': books,
         'numbers': numbers,
     }
-    return HttpResponse(template.render(biblio_data))
+    return HttpResponse(template.render(biblio_data, request))
+
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            book.copy_count += 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if not book_id:
+            return redirect('/index/')
+        else:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()
+        return redirect('/index/')
+    else:
+        return redirect('/index/')
