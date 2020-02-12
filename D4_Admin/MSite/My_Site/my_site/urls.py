ce0167682eaecb12a19fafcadbe677c8a503b24e
stalkspectrum <55536946+stalkspectrum@users.noreply.github.com>
2020-02-12 21:45:25 +0800

    D4.11 fix 12.2145

--- D4_Admin/MSite/My_Site/my_site/urls.py
+++ D4_Admin/MSite/My_Site/my_site/urls.py
@@ -20,6 +20,7 @@ from p_library import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.books_list),
+    path('publishers/', views.publishers_list),
     path('index/', views.index),
     path('index/book_increment/', views.book_increment),
     path('index/book_decrement/', views.book_decrement),
